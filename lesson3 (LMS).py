b7dc04bbc861a395d935c1241363c6bf79660c21
Bohdan Bylo <67965988+BVBohdan@users.noreply.github.com>
2021-11-20 00:27:53 +0200

    Initial commit

---
+++ lesson3 (LMS).py
@@ -0,0 +1,25 @@
+# Task 1   “Good day <name>! <day> is a perfect day to learn some python.”
+name = 'Bohdan'
+day = 'Saturday'
+print('Good day', name, '!', day, 'is a perfect day to learn some python.')
+# Task 2 Manipulate strings.
+name = 'Bohdan'
+surname = 'Bylo'
+print('Hello', name + '', surname)
+# Task 3 Using python as a calculator.
+a = 7
+b = 4
+add = a + b
+sub = a - b
+div = a / b
+mult = a * b
+exp = a ** b
+mod = a % b
+fl_div = a // b
+print (add)
+print (sub)
+print (div)
+print (mult)
+print (exp)
+print (mod)
+print (fl_div)
