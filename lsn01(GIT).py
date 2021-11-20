34858f55ab89f476e83b617bf12d1edac693b30c
Bohdan Bylo <67965988+BVBohdan@users.noreply.github.com>
2021-11-20 11:28:32 +0200

    Lesson2 (GIT)

--- lsn01(GIT).py
+++ lsn01(GIT).py
@@ -10,13 +10,13 @@ print(dist)
 
 # задание Создайте переменные Высоты ширины и глубины для прямоугольника
 h = 6  # высота
-w = 8  # ширина
+w = 9  # ширина
 d = 12  # глубина
 s = ((h**2 + w**2)**(0.5))*d   # площадь диагонального сечения прямоугольного параллелепипеда
 s_shifer = 3*4  # площадь куска шифера
 cost_shifer = 600  # стоимость куска шифера
 right_amount = s // s_shifer  # необходимое колличество шифера
-general_cost = (right_amount / s_shifer)*cost_shifer  # общая стоимость (сумма)
+general_cost = right_amount*cost_shifer  # общая стоимость (сумма)
 remains = (s / s_shifer) - (s // s_shifer)  # остатки шифера
 print(right_amount)
 print(general_cost)
