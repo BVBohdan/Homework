af3584e9306506f88c443c40e3579e5abbc57035
Bohdan Bylo <67965988+BVBohdan@users.noreply.github.com>
2021-11-25 18:40:30 +0200

    Lesson 5 LMS

--- lsn05(LMS).py
+++ lsn05(LMS).py
@@ -1,4 +1,4 @@
-
+#                                                       Lesson 5 LMS
 # Task 1. The Guessing Game.
 # Write a program that generates a random number between 1 and 10 and lets the user guess what number was generated.
 # The result should be sent back to the user via a print statement.
